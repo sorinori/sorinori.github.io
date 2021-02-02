79a05ac8c3ee0316cc0fa2350a86d546e1a80854
Ali Rostami <rostamiev@gmail.com>
2021-02-02 19:46:17 +0100

    texts added

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 $.get('text1.txt', function(data) {
    $('#text1').html(data);
 }, 'text');
+$.get('text2.txt', function(data) {
+   $('#text2').html(data);
+}, 'text');
 //$("#main_list").append("");
