64fef40e34ef2e311afceb5d8209d2a15f27f388
Ali Rostami <rostamiev@gmail.com>
2021-02-02 20:06:37 +0100

    all texts files added

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-$.get('text1.txt', function(data) {
+function change_lang() {
+  var x = document.getElementById("langselect").value;
+  var tmp = 'en'
+  if(x == "Korean") tmp = 'kr'
+  if(x == "Persian") tmp = 'fa'
+  
+  $.get('text1_' + tmp + '.txt', function(data) {
+   $('#text1').html(data);
+  }, 'text');
+  
+  $.get('text2_' + tmp + '.txt', function(data) {
+    $('#text2').html(data);
+  }, 'text');
+
+}
+
+$.get('text1_en.txt', function(data) {
    $('#text1').html(data);
 }, 'text');
-$.get('text2.txt', function(data) {
+$.get('text2_en.txt', function(data) {
    $('#text2').html(data);
 }, 'text');
-//$("#main_list").append("");
+
+
+
