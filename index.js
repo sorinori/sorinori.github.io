e6de0ac052cd6069a1268007da8e58f4cbc7b8bf
Ali Rostami <rostamiev@gmail.com>
2021-02-02 19:39:11 +0100

    texts added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-ctx.font = "30px Arial";
+ctx.font = "12 Arial";
 $.get('text1.txt', function(data) {
    //$('#text1').html(data);
    ctx.fillText(data,10,50);
