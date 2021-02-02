bed22ea1c22978becfe6d90f76c48bf9559febe4
Ali Rostami <rostamiev@gmail.com>
2021-02-02 19:29:05 +0100

    texts added

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+var canvas = document.getElementById("myCanvas");
+var ctx = canvas.getContext("2d");
+ctx.font = "30px Arial";
 $.get('text1.txt', function(data) {
-   $('#text1').html(data);
+   //$('#text1').html(data);
+   ctx.fillText(data,10,50);
 }, 'text');
 //$("#main_list").append("");
