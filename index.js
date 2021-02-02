0bc18c26bff307e79664579d4542410832ef7515
Ali Rostami <rostamiev@gmail.com>
2021-02-02 21:07:56 +0100

    all texts files added

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 function change_lang() {
   var x = document.getElementById("langselect").value;
   var tmp = 'en'
-  if(x == "Korean") tmp = 'kr'
-  if(x == "Persian") tmp = 'fa'
+  if(x == "Korean") {
+    tmp = 'kr';
+  }
+  
+  if(x == "Persian") {
+   tmp = 'fa';
+   $("#text_title_1").html('موسیقی در کره');
+   $("#text_title_2").html('موسیقی در ایران');
+   
+  }
+  
   
   $.get('text1_' + tmp + '.txt', function(data) {
    $('#text1').html(data);
