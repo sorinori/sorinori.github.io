53c2dad72d552e0b838297010d21440ff9992e10
Ali Rostami <rostamiev@gmail.com>
2021-02-03 20:45:09 +0100

    small change

--- index.js
+++ index.js
@@ -4,19 +4,24 @@ function change_lang() {
 
   if(x == "English") {
     tmp = 'en';
+    $('li1').css('text-align:left');
+    $('li2').css('text-align:left'); 
     $("#text_title_1").html("Music in Korea");
     $("#text_title_2").html("Music in Iran");
   }
 
   if(x == "Korean") {
     tmp = 'kr';
-     
+    $('li1').css('text-align:left');
+    $('li2').css('text-align:left'); 
     $("#text_title_1").html("한국의 음악");
     $("#text_title_2").html("이란의 음악");
   }
   
   if(x == "Persian") {
    tmp = 'fa';
+   $('li1').css('text-align:right');
+   $('li2').css('text-align:right');
    $("#text_title_1").html('موسیقی در کره');
    $("#text_title_2").html('موسیقی در ایران');
   }
