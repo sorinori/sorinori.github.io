074801b17f4e8b0829b11a1e257cc24f16e0ed23
Ali Rostami <rostamiev@gmail.com>
2021-02-15 15:35:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ function change_lang(x) {
     $("#text_title_1").html("Music Talk with Jinjoo from South Korea");
     $("#text_title_2").html("Music Takk with Ali from Iran");
     $("#text_title_3").html("Music Takk with Brian from the US");
+    $("#item1p").html("Music Talk with Jinjoo from South Korea");
+    $("#item2p").html("Music Takk with Ali from Iran");
+    $("#item3p").html("Music Takk with Brian from the US");
   }
 
   if(x == "Korean") {
@@ -20,6 +23,7 @@ function change_lang(x) {
     $('#li2').css('direction','ltr');
     $("#text_title_1").html("한국의 음악");
     $("#text_title_2").html("이란의 음악");
+    $("#text_title_3").html("이란의 음악");
   }
   
   if(x == "Persian") {
@@ -31,6 +35,9 @@ function change_lang(x) {
    $("#text_title_1").html('صحبت موسیقایی با جینجو از کره جنوبی');
    $("#text_title_2").html('صحبت موسیقایی با علی از ایران');
    $("#text_title_3").html('صحبت موسیقایی با برایان از آمریکا');
+   $("#item1p").html('صحبت موسیقایی با جینجو از کره جنوبی');
+   $("#item2p").html('صحبت موسیقایی با علی از ایران');
+   $("#item3p").html('صحبت موسیقایی با برایان از آمریکا');
   }
   
   if(x == "Spanish") {
@@ -42,6 +49,9 @@ function change_lang(x) {
    $("#text_title_1").html('Charla musical con Jinjoo, de Corea del Sur');
    $("#text_title_2").html('Charla musical con Ali, de Iran');
    $("#text_title_3").html('Charla musical con Brian, de US');
+   $("#item1p").html('Charla musical con Jinjoo, de Corea del Sur');
+   $("#item2p").html('Charla musical con Ali, de Iran');
+   $("#item3p").html('Charla musical con Brian, de US');
   }
   
   if(x == "German") {
@@ -53,6 +63,9 @@ function change_lang(x) {
    $("#text_title_1").html('Musikgespräch mit Jinjoo aus Südkorea');
    $("#text_title_2").html('Musikgespräch mit Ali aus Iran');
    $("#text_title_3").html('Musikgespräch mit Brian aus vereinigte staaten');
+   $("#item1p").html('Musikgespräch mit Jinjoo aus Südkorea');
+   $("#item2p").html('Musikgespräch mit Ali aus Iran');
+   $("#item3p").html('Musikgespräch mit Brian aus vereinigte staaten');
   }
   
   
