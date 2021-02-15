d762512073bd1bab6cc440dba082acdbca44494f
Ali Rostami <rostamiev@gmail.com>
2021-02-15 15:41:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function change_lang(x) {
     $('#li2').css('text-align','left'); 
     $('#li1').css('direction','ltr');
     $('#li2').css('direction','ltr'); 
+    $('#li3').css('direction','ltr');
     $("#text_title_1").html("Music Talk with Jinjoo from South Korea");
     $("#text_title_2").html("Music Takk with Ali from Iran");
     $("#text_title_3").html("Music Takk with Brian from the US");
@@ -21,6 +22,7 @@ function change_lang(x) {
     $('#li2').css('text-align','left'); 
     $('#li1').css('direction','ltr');
     $('#li2').css('direction','ltr');
+    $('#li3').css('direction','ltr');
     $("#text_title_1").html("한국의 음악");
     $("#text_title_2").html("이란의 음악");
     $("#text_title_3").html("이란의 음악");
@@ -32,6 +34,7 @@ function change_lang(x) {
    $('#li2').css('text-align','right');
    $('#li1').css('direction','rtl');
    $('#li2').css('direction','rtl'); 
+   $('#li3').css('direction','rtl');
    $("#text_title_1").html('صحبت موسیقایی با جینجو از کره جنوبی');
    $("#text_title_2").html('صحبت موسیقایی با علی از ایران');
    $("#text_title_3").html('صحبت موسیقایی با برایان از آمریکا');
@@ -46,6 +49,7 @@ function change_lang(x) {
    $('#li2').css('text-align','left');
    $('#li1').css('direction','ltr');
    $('#li2').css('direction','ltr'); 
+   $('#li3').css('direction','ltr'); 
    $("#text_title_1").html('Charla musical con Jinjoo, de Corea del Sur');
    $("#text_title_2").html('Charla musical con Ali, de Iran');
    $("#text_title_3").html('Charla musical con Brian, de US');
@@ -60,6 +64,7 @@ function change_lang(x) {
    $('#li2').css('text-align','left');
    $('#li1').css('direction','ltr');
    $('#li2').css('direction','ltr'); 
+   $('#li3').css('direction','ltr'); 
    $("#text_title_1").html('Musikgespräch mit Jinjoo aus Südkorea');
    $("#text_title_2").html('Musikgespräch mit Ali aus Iran');
    $("#text_title_3").html('Musikgespräch mit Brian aus vereinigte staaten');
